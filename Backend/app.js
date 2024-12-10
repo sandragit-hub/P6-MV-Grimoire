f38670780823db48b6aa229ec6a99a11d5832888
Sandra <sandra.rossi17@hotmail.com>
2024-12-10 21:12:49 +0100

    configuration des routes (put,post,get,delete)

--- Backend/app.js
+++ Backend/app.js
@@ -3,16 +3,44 @@ const app = express();
 const mongoose = require('mongoose');
 const Book = require('./models/Book');
 
-app.use(express.json());
+app.use(express.json()); //intercepte toute les requete avec content type json
 
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
+app.post('/api/books', (req, res, next) => {
     const Book = new Book({
         ...req.body
     });
     Book.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id'), (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(201).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+app.put('/api/books/:id'), (req, res, next) => {
+    Book.updateOne({ _id: req.params.id })
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+app.get('/api/books/bestrating', (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/books', (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
         .catch(error => res.status(400).json({ error }));
 });
 
