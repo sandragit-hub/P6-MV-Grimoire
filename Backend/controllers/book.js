d9e394bc5b23e94bfb545476237a143b2c0b0915
Sandra <sandra.rossi17@hotmail.com>
2024-12-11 21:54:14 +0100

    finalisation des routes et création des controlleurs

---
+++ Backend/controllers/book.js
@@ -0,0 +1,40 @@
+const Book = require('../models/Book');
+
+exports.createBook = (req, res, next) => {
+    const Book = new Book({
+        ...req.body
+    });
+    Book.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteBook = (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(201).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifyOneBook = (req, res, next) => {
+    Book.updateOne({ _id: req.params.id })
+        .then(() => res.status(201).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.ratingBook = (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllBook = (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
+};
--- Backend/route/book.js
+++ Backend/route/book.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const bookCtrl = require('..controllers/book');
 
 
-
-
-
-
-
+router.post('/', bookCtrl.createBook);
+router.delete('/:id', bookCtrl.deleteBook);
+router.put('/:id', bookCtrl.modifyOneBook);
+router.get('/bestrating', bookCtrl.ratingBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllBook);
 
 module.exports = router;
