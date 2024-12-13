03d204d7c9bb38d2f37ddcc74a508703426762e7
Sandra <sandra.rossi17@hotmail.com>
2024-12-13 17:11:23 +0100

    modif 3000=>4000 + correction erreur controlleur signup + réinstallation de nodemon

--- Backend/server.js
+++ Backend/server.js
@@ -14,11 +14,11 @@ const normalizePort = val => {
     return false;
 };
 
-const port = normalizePort(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || 4000);
 app.set('port', port);
 
 const errorHandler = error => {
-    if (error.sycall != 'listen') {
+    if (error.syscall != 'listen') {
         throw error;
     }
     const address = server.address();
@@ -38,7 +38,7 @@ const server = http.createServer(app);
 server.on('error', errorHandler);
 server.on('listening', () => {
     const address = server.address();
-    const bind = typeof adress === 'string' ? 'pipe' + adress : 'port' + port;
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
     console.log('Listening on' + bind);
 });
 
