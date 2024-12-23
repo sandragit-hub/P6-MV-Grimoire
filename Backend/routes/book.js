9b237c89f82170f5fa384b6cd35f8f715da50678
Sandra <sandra.rossi17@hotmail.com>
2024-12-23 10:50:44 +0100

    modi route creatbook et modif

--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config')
 const bookCtrl = require('../controllers/book');
